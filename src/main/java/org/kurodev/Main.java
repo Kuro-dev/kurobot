56a7b6048a33c427f6236292554f275472bb2feb
kuro-dev <dirkreher@ymail.com>
2021-01-29 20:41:46 +0100

    Added presence activity, displaying help command

--- src/main/java/org/kurodev/Main.java
+++ src/main/java/org/kurodev/Main.java
@@ -2,9 +2,10 @@ package org.kurodev;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import org.kurodev.conf.MySettings;
 import org.kurodev.conf.Setting;
-import org.kurodev.events.MessageEvent;
+import org.kurodev.events.MessageEventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,8 +30,11 @@ public class Main {
 
     private static void startBot() throws InterruptedException, LoginException {
         JDA jda = JDABuilder.createDefault(SETTINGS.getSetting(Setting.TOKEN)).build();
-        jda.addEventListener(new MessageEvent());
+        MessageEventHandler event = new MessageEventHandler();
+        event.initialize();
+        jda.addEventListener(event);
         jda.awaitReady();
+        jda.getPresence().setActivity(Activity.of(Activity.ActivityType.LISTENING,"!k help"));
     }
 
     private static void loadSettings() throws IOException {
