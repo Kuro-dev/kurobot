0b1342340914786776646acee5780a0a55a83b26
kuro-dev <dirkreher@ymail.com>
2021-01-29 20:38:58 +0100

    Added additional features to insult command: If used alone it will insult you if used with a @mention it will target that mention - Currently only targets the first mention. - TODO: target every mention given

--- src/main/java/org/kurodev/command/standard/impl/InsultCommand.java
+++ src/main/java/org/kurodev/command/standard/impl/InsultCommand.java
@@ -1,9 +1,10 @@
-package org.kurodev.command.commands.impl;
+package org.kurodev.command.standard.impl;
 
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
-import org.kurodev.command.commands.Command;
+import org.kurodev.command.standard.Command;
 import org.kurodev.events.InsultHandler;
 
 import java.io.IOException;
@@ -15,12 +16,21 @@ public class InsultCommand extends Command {
     private final InsultHandler insults;
 
     public InsultCommand(InsultHandler insults) {
-        super("insultMe");
+        super("insult");
         this.insults = insults;
     }
 
     @Override
     public void execute(TextChannel channel, String[] args, @NotNull GuildMessageReceivedEvent event) throws IOException {
-        insults.execute(event);
+        if (containsMention(event)) {
+            //TODO target every mention given
+            insults.execute(event, event.getMessage().getMentions().get(0));
+        } else {
+            insults.execute(event);
+        }
+    }
+
+    private boolean containsMention(GuildMessageReceivedEvent event) {
+        return !event.getMessage().getMentions(Message.MentionType.USER).isEmpty();
     }
 }
