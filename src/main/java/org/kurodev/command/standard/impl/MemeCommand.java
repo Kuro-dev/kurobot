7d1bff68711128ab52aceacb762f275b8079a2ec
kuro-dev <dirkreher@ymail.com>
2021-01-29 20:42:54 +0100

    Cleaned up constructor and moved actual "calculating" code to newly added prepare() method. - cleaned up class overall, removed nested if statements and unnecessary conditions

--- src/main/java/org/kurodev/command/standard/impl/MemeCommand.java
+++ src/main/java/org/kurodev/command/standard/impl/MemeCommand.java
@@ -1,10 +1,10 @@
-package org.kurodev.command.commands.impl;
+package org.kurodev.command.standard.impl;
 
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 import org.kurodev.Main;
-import org.kurodev.command.commands.Command;
+import org.kurodev.command.standard.Command;
 import org.kurodev.conf.Setting;
 
 import java.io.IOException;
@@ -19,32 +19,27 @@ import java.util.stream.Collectors;
  **/
 public class MemeCommand extends Command {
     Path memeFolder = Paths.get(Main.getSettings().getSetting(Setting.MEME_FOLDER));
-    private boolean loadSuccess;
-    private long files;
 
     public MemeCommand() {
         super("meme");
-        try {
-            files = Files.list(memeFolder).filter(Files::isRegularFile).count();
-            logger.info("Successfully loaded meme folder");
-            loadSuccess = true;
-        } catch (IOException e) {
-            logger.error("Failed to load meme folder", e);
-            loadSuccess = false;
+    }
+
+    @Override
+    public void prepare() throws Exception {
+        if (!Files.exists(memeFolder)) {
+            logger.info("Creating Meme Folder");
+            Files.createDirectories(memeFolder);
         }
     }
 
     @Override
     public void execute(TextChannel channel, String[] args, @NotNull GuildMessageReceivedEvent event) throws IOException {
-        if (loadSuccess){
-            channel.sendMessage("There you go").complete();
-            channel.sendFile(getRandomImage().toFile()).complete();
-        }else {
-            channel.sendMessage("Failed to load meme database, please try again once the bot has restarted or contact the developer").queue();
-        }
+        channel.sendMessage("There you go").complete();
+        channel.sendFile(getRandomImage().toFile()).complete();
     }
 
     private Path getRandomImage() throws IOException {
+        final long files = Files.list(memeFolder).filter(Files::isRegularFile).count();
         return Files.list(memeFolder).filter(Files::isRegularFile).collect(Collectors.toList()).get(new Random().nextInt(Math.toIntExact(files)));
     }
 }
