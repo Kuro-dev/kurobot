eb9da704971b3e57fa165bf7dae2edcab414067c
kuro-dev <dirkreher@ymail.com>
2021-01-29 20:44:24 +0100

    added javadoc and comments

--- src/main/java/org/kurodev/UserIDs.java
+++ src/main/java/org/kurodev/UserIDs.java
@@ -1,17 +1,19 @@
 package org.kurodev;
 
 /**
+ * A list of important or special userIDs for use in JDA
  * @author kuro
  **/
 public enum UserIDs {
+    //Admin
     kuro("223878679061725185"),
+    //some guy I just want to insult everytime he writes something.
     Mau("290501224455995392"),
     ;
 
     private final String id;
 
     UserIDs(String id) {
-
         this.id = id;
     }
 
