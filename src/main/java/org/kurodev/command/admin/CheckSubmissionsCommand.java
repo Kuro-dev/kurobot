4038d1727be5d99a3cc0f985ad39bb482c5c47ad
kuro-dev <dirkreher@ymail.com>
2021-01-30 17:04:05 +0100

    Added checkSubmissions command to show statistics of whether or not there are any new submissions.

---
+++ src/main/java/org/kurodev/command/admin/CheckSubmissionsCommand.java
@@ -0,0 +1,62 @@
+package org.kurodev.command.admin;
+
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import org.jetbrains.annotations.NotNull;
+import org.kurodev.Main;
+import org.kurodev.config.Setting;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author kuro
+ **/
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class CheckSubmissionsCommand extends AdminCommand {
+    Path insultSubmissions, memeSubmissions;
+
+    public CheckSubmissionsCommand() {
+        super("checkSubmissions");
+        insultSubmissions = Paths.get(Main.SETTINGS.getSetting(Setting.INSULT_SUBMISSIONS));
+        memeSubmissions = Paths.get(Main.SETTINGS.getSetting(Setting.MEME_SUBMISSIONS));
+    }
+
+    @Override
+    public void execute(TextChannel channel, String[] args, @NotNull GuildMessageReceivedEvent event) throws IOException {
+        MessageAction msg = channel.sendMessage("Submission statistics:\n");
+        int memeFiles = readFiles(memeSubmissions);
+        int insultFiles = readFiles(insultSubmissions);
+        int insultLines = readInsultLines();
+        msg.append(String.format("Meme Files: %d\n", memeFiles));
+        msg.append(String.format("Insult Files: %d, insult lines: %d\n", insultFiles, insultLines));
+        int total = memeFiles + insultFiles, absoluteTotal = total + insultLines;
+        msg.append(String.format("Total: %d (%d)", total, absoluteTotal));
+        msg.queue();
+    }
+
+
+    private int readFiles(Path path) throws IOException {
+        return (int) Files.walk(path).filter(Files::isRegularFile).count();
+    }
+
+    private int readInsultLines() {
+        try {
+            return (int) Files.walk(insultSubmissions).filter(Files::isRegularFile).mapToInt(value -> {
+                try {
+                    int ret = Files.readAllLines(value).size();
+                    return ret;
+                } catch (IOException e) {
+                    logger.error("Could not read file: " + value, e);
+                    return 0;
+                }
+            }).sum();
+        } catch (IOException e) {
+            logger.error("failed to retrieve files from submissions", e);
+            return 0;
+        }
+    }
+}
