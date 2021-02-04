601302e5e505397d48d88d7e18de916321b0ece0
kuro-dev <dirkreher@ymail.com>
2021-02-04 19:44:52 +0100

    Major refactoring in preparation of new command structure. - Renamed Command.java to GuildCommand.java - Added Command.java Interface - Major code cleanup for Help Command - Moved every Guildcommand to package guild

---
+++ src/main/java/org/kurodev/command/guild/GuildCommand.java
@@ -0,0 +1,34 @@
+package org.kurodev.command.guild;
+
+import net.dv8tion.jda.api.entities.PrivateChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+import org.kurodev.command.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author kuro
+ **/
+public abstract class GuildCommand implements Command {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    private final String command;
+
+    public GuildCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public boolean check(String command, @NotNull GuildMessageReceivedEvent event) {
+        return this.command.equalsIgnoreCase(command);
+    }
+
+    public abstract void execute(TextChannel channel, String[] args, @NotNull GuildMessageReceivedEvent event) throws IOException;
+
+}
