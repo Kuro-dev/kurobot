96d95e58bec5294ee809c12c96f4f4925c9aca63
kuro-dev <dirkreher@ymail.com>
2021-02-24 12:28:28 +0100

    minor code cleanup

--- src/main/java/org/kurodev/command/guild/admin/InfoCommand.java
+++ src/main/java/org/kurodev/command/guild/admin/InfoCommand.java
@@ -1,11 +1,10 @@
-package org.kurodev.command.guild.standard;
+package org.kurodev.command.guild.admin;
 
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import org.jetbrains.annotations.NotNull;
 import org.kurodev.Main;
-import org.kurodev.command.guild.admin.AdminCommand;
 import org.kurodev.config.Setting;
 
 /**
@@ -19,12 +18,12 @@ public class InfoCommand extends AdminCommand {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
     public void execute(TextChannel channel, String[] args, @NotNull GuildMessageReceivedEvent event) {
-        MessageAction msg = channel.sendMessage("These are the settings:\n");
+        MessageAction msg = channel.sendMessage("```\nThese are the settings:\n");
         for (Setting value : Setting.values()) {
             if (!"token".equals(value.getKey()))
-                msg.append(value.getKey()).append("=").append(Main.SETTINGS.getSetting(value)).append("\n");
+                msg.append(value.getKey()).append(" = ").append(Main.SETTINGS.getSetting(value)).append("\n");
         }
-        msg.queue();
+        msg.append("```").queue();
     }
 
     @Override
