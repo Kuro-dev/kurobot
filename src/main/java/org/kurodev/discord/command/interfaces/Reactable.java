77dbac364c4ef06e15c35c58968e515e15758c6c
kuro-dev <dirkreher@ymail.com>
2021-03-26 11:11:38 +0100

    Added Javadoc - Moved to command.interfaces package

--- src/main/java/org/kurodev/discord/command/interfaces/Reactable.java
+++ src/main/java/org/kurodev/discord/command/interfaces/Reactable.java
@@ -1,9 +1,14 @@
-package org.kurodev.discord.command.guild;
+package org.kurodev.discord.command.interfaces;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEvent;
+import org.kurodev.discord.command.guild.GuildCommandHandler;
 
+/**
+ * If the command can support reactions, simply make it implement this interface, the rest will happen automatically.
+ * (don't forget to add your command to {@link GuildCommandHandler#prepare()})
+ */
 public interface Reactable {
     void onReact(Message reactedMessage, GuildMessageReactionAddEvent event);
 
