f5f3eca8ad310dde93c08cd9860b9e6041219513
Kuro <dev_kuro@yahoo.de>
2021-05-31 20:50:52 +0200

    Changed version from 1.7.1 to to 1.8.0 deleted deprecated argument handling classes

--- src/main/java/org/kurodev/discord/message/command/guild/GuildCommand.java
+++ src/main/java/org/kurodev/discord/message/command/guild/GuildCommand.java
@@ -4,10 +4,8 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.apache.commons.cli.CommandLine;
 import org.jetbrains.annotations.NotNull;
-import org.kurodev.discord.message.command.argument.Argument;
 import org.kurodev.discord.message.command.generic.CommandType;
 import org.kurodev.discord.message.command.generic.GenericCommand;
 
@@ -34,13 +32,4 @@ public abstract class GuildCommand extends GenericCommand {
 
     protected abstract void execute(TextChannel channel, CommandLine args, MessageReceivedEvent event) throws IOException;
 
-    /**
-     * Final removal in 1.8.0
-     *
-     * @deprecated Replaced by {@link #execute(TextChannel, CommandLine, GuildMessageReceivedEvent)}
-     */
-    @Deprecated(forRemoval = true, since = "1.7.0")
-    public void execute(TextChannel channel, Argument args, @NotNull GuildMessageReceivedEvent event) throws IOException {
-
-    }
 }
