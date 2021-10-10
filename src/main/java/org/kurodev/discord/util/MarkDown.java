6ae87885387110b8339ac97221aa0a18e016b037
Kuro <dev_kuro@yahoo.com>
2021-10-10 17:45:56 +0200

    Code cleanup of Markdown class

--- src/main/java/org/kurodev/discord/util/MarkDown.java
+++ src/main/java/org/kurodev/discord/util/MarkDown.java
@@ -1,42 +1,42 @@
 package org.kurodev.discord.util;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * @author kuro
  **/
-//TODO clean this up
 public enum MarkDown {
     ITALICS("*"),
     BOLD("**"),
     UNDERLINE("__"),
     STRIKETHROUGH("~~"),
-    QUOTE("> ", false),
+    QUOTE("> ", " "),
     CODE("`"),
-    CODE_BLOCK("```\n"),
+    CODE_BLOCK("```\n", "\n```"),
     ;
 
-    private final String markdown;
-    private final boolean bothSides;
+    private final String markdownStart;
+    private final String markdownEnd;
 
-    MarkDown(String s) {
-        this(s, true);
+    MarkDown(String start) {
+        this(start, start);
     }
 
 
-    MarkDown(String s, boolean bothSides) {
-        this.markdown = s;
-        this.bothSides = bothSides;
+    MarkDown(String s, String end) {
+        this.markdownStart = s;
+        this.markdownEnd = end;
     }
 
     public String wrap(String content) {
         boolean isTextBlock = content.contains("\n") || content.contains("\r");
         if (isTextBlock && this == QUOTE) {
-            StringBuilder builder = new StringBuilder();
-            for (String line : content.split("\n|\r")) {
-                builder.append(markdown).append(line).append(bothSides ? markdown : "").append("\n");
-            }
-            return builder.toString();
+            return Arrays.stream(content.split("[\n\r]"))
+                    .map(line -> markdownStart + line + markdownEnd + "\n")
+                    .collect(Collectors.joining());
         } else {
-            return markdown + content + (bothSides ? markdown : "");
+            return markdownStart + content + (markdownEnd);
         }
     }
 
